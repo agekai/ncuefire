d22d7213349348af69443ae637273aee274a7e10
agekai <kai0753012@gmail.com>
2025-05-16 16:31:06 +0800

    Update mysketch.js

--- scenes/scene1/mysketch.js
+++ scenes/scene1/mysketch.js
@@ -10,6 +10,7 @@ let dotFlashes = [];
 let showAxes = false;
 
 function setup() {
+  canvas.elt.style.touchAction = 'manipulation';
   let container = document.getElementById('canvas-container');
   canvas = createCanvas(600, 600);
   canvas.parent(container);
