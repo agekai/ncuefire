f01698a0dda7a19f1b852e9ef3520966db8d81b8
agekai <kai0753012@gmail.com>
2025-05-16 15:12:22 +0800

    Update mysketch.js

--- scenes/scene5/mysketch.js
+++ scenes/scene5/mysketch.js
@@ -12,16 +12,29 @@ let dotCommaLines = [
   " 	點點點",
   " 點點點點點",
   " 點點點點點",
-  "    點點點逗",
-  "            逗",
-  "          逗",
-  "        逗"
+  "    點點點點",
+  "            點",
+  "          點",
+  "        點"
 ];
 
-// 跳舞的「點」位置：[row, col]
+// 跳舞的「逗」位置
 let dancingDots = [
-  [0, 2],[0, 4],[2, 1],[2, 5],[3, 4],[3, 5],[3, 6],	
+  [0, 2], [0, 4], [2, 1], [2, 5], [3, 4], [3, 5], [3, 6],
 ];
+
+// 拖曳用物件管理三個字
+let draggableNumbers = [
+  { char: "4", x: baseWidth * 0.75, y: baseHeight * 0.85, col: null, dancing: false },
+  { char: "5", x: baseWidth * 0.85, y: baseHeight * 0.85, col: null, dancing: false },
+  { char: "3", x: baseWidth * 0.65, y: baseHeight * 0.85, col: null, dancing: false },
+];
+
+let draggingIndex = -1;
+let dragOffsetX = 0;
+let dragOffsetY = 0;
+
+let isDouDancing = false;
 
 function setup() {
   let container = document.getElementById('canvas-container');
@@ -39,6 +52,16 @@ function setup() {
 
   adjustCanvasSize();
   window.addEventListener('resize', adjustCanvasSize);
+
+  draggableNumbers[0].col = color(255, 100, 100);
+  draggableNumbers[1].col = color(100, 255, 100);
+  draggableNumbers[2].col = color(100, 100, 255);
+
+  canvas.mousePressed(startDragging);
+  canvas.mouseReleased(stopDragging);
+  canvas.touchStarted(startDragging);
+  canvas.touchEnded(stopDragging);
+  canvas.touchMoved(touchDragging);
 }
 
 function adjustCanvasSize() {
@@ -51,7 +74,6 @@ function adjustCanvasSize() {
 
 function draw() {
   background(0);
-
   let scaleFactor = canvasSize / baseWidth;
   push();
   scale(scaleFactor);
@@ -59,14 +81,17 @@ function draw() {
   drawFrame();
 
   push();
-    drawDotArt();
+  drawDotArt();
   pop();
-	
+
   push();
-    translate(100, baseHeight * 0.75);
-    drawAxesText();
-    drawFireText();
+  translate(100, baseHeight * 0.75);
+  drawAxesText();
+  drawFireText();
   pop();
+
+  updateDragging();
+  drawDraggableNumbers();
 
   pop();
 }
@@ -94,13 +119,11 @@ function drawAxesText() {
   noStroke();
 
   let exclusionRadius = 50;
-
   for (let i = -baseWidth + 520; i < baseWidth - 120; i += 20) {
     if (dist(i, 0, 0, 0) > exclusionRadius - 20 && i !== 0) {
       text("點,", i + 10, 0);
     }
   }
-
   for (let j = -baseHeight + 180; j < baseHeight - 460; j += 20) {
     if (dist(0, j, 0, 0) > exclusionRadius && j !== 0 && j !== -20) {
       text("點", 10, j);
@@ -150,15 +173,14 @@ function drawDotArt() {
 
     for (let j = 0; j < dotCommaLines[i].length; j++) {
       let char = dotCommaLines[i][j];
-
-      let isDancing = dancingDots.some(([row, col]) => row === i && col === j);
+      let isDancingDot = dancingDots.some(([row, col]) => row === i && col === j);
 
       if (char == "點") {
-        if (isDancing) {
+        if (isDouDancing && isDancingDot) {
           let flicker = map(sin(frameCount * 0.2 + i + j), -1, 1, 100, 255);
           fill(color(255, 255, 0, flicker));
           let bounce = sin(frameCount * 0.3 + i + j) * 1;
-          text(char, x, y + bounce);
+          text("逗", x, y + bounce);
         } else {
           fill(255);
           text(char, x, y);
@@ -183,4 +205,72 @@ function typeWriter() {
     annotationIndex++;
     setTimeout(typeWriter, annotationSpeed);
   }
+}
+
+function startDragging() {
+  let scaleFactor = canvasSize / baseWidth;
+  let mx = (touches.length > 0 ? touches[0].x : mouseX) / scaleFactor;
+  let my = (touches.length > 0 ? touches[0].y : mouseY) / scaleFactor;
+
+  for (let i = 0; i < draggableNumbers.length; i++) {
+    let d = dist(mx, my, draggableNumbers[i].x, draggableNumbers[i].y);
+    if (d < 20) {
+      draggingIndex = i;
+      dragOffsetX = draggableNumbers[i].x - mx;
+      dragOffsetY = draggableNumbers[i].y - my;
+      return false;
+    }
+  }
+}
+
+function stopDragging() {
+  draggingIndex = -1;
+}
+
+function updateDragging() {
+  if (draggingIndex !== -1 && touches.length === 0) {
+    let scaleFactor = canvasSize / baseWidth;
+    let mx = mouseX / scaleFactor;
+    let my = mouseY / scaleFactor;
+
+    draggableNumbers[draggingIndex].x = constrain(mx + dragOffsetX, 0, baseWidth);
+    draggableNumbers[draggingIndex].y = constrain(my + dragOffsetY, 0, baseHeight);
+  }
+
+  let allInFirstQuadrant = true;
+  for (let num of draggableNumbers) {
+    num.dancing = (num.x > baseWidth * 0.2 && num.y < baseHeight * 0.72);
+    if (!num.dancing) allInFirstQuadrant = false;
+  }
+
+  isDouDancing = allInFirstQuadrant;
+}
+
+function drawDraggableNumbers() {
+  textSize(32);
+  textAlign(CENTER, CENTER);
+  noStroke();
+
+  for (let num of draggableNumbers) {
+    if (num.dancing) {
+      let bounce = sin(frameCount * 0.3 + num.char.charCodeAt(0)) * 5;
+      fill(255, 255, 0);
+      text(num.char, num.x, num.y + bounce);
+    } else {
+      fill(num.col);
+      text(num.char, num.x, num.y);
+    }
+  }
+}
+
+function touchDragging() {
+  if (touches.length > 0 && draggingIndex !== -1) {
+    let scaleFactor = canvasSize / baseWidth;
+    let tx = touches[0].x / scaleFactor;
+    let ty = touches[0].y / scaleFactor;
+
+    draggableNumbers[draggingIndex].x = constrain(tx + dragOffsetX, 0, baseWidth);
+    draggableNumbers[draggingIndex].y = constrain(ty + dragOffsetY, 0, baseHeight);
+  }
+  return false; // 防止頁面滑動
 }
